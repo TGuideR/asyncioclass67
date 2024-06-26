cfd3c1eb1603db3dc7a69b9fa0255da0dac73cd2
TGuideR <thanakorng2147@gmail.com>
2024-06-26 12:22:47 +0700

    ส่งงาน

--- assignment3/asyncio02.py
+++ assignment3/asyncio02.py
@@ -1,2 +1,18 @@
 # example of running a coroutine
+import asyncio
+#define a coroutine
+async def custom_coro():
+    #await another coroutine
+    await asyncio.sleep(5)
+    # print('Done')
 
+# main coroutine
+async def main():
+    # execute my custom coroutine
+    await custom_coro()
+    # print('Wait a sec')
+    # await asyncio.sleep(3)
+    # print('Custom done so I am')
+
+# start the coroutine program
+asyncio.run(main())
